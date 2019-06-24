e1f7e85918dc5978d275eb844ebb451037dab597
Walt Mankowski <waltman@pobox.com>
2019-06-24 16:12:55 -0400

    another draft. still not working!

--- challenge_014/van_eck.pl
+++ challenge_014/van_eck.pl
@@ -4,10 +4,21 @@ use warnings;
 use feature 'say';
 
 my @seen;
+my %first;
 my $val = 0;
+$first{0} = 1;
 
 for my $n (0..10) {
     say "$n $val";
-    $val = exists $seen[$val] ? $val = $n - $seen[$val] : 0;
+    if (defined $first{$val}) {
+        $val = 0;
+        delete $first{$val};
+    } elsif (exists $seen[$val]) {
+        $val = $n - $seen[$val];
+    } else {
+        $first{$val} = 1;
+        $val = 0;
+    }
+#    $val = exists $seen[$val] ? $val = $n - $seen[$val] : 0;
     $seen[$val] = $val;
 }
